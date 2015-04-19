e9a2bdc7d65068d45424d5030d1ff9ba11e0b829
drawsgood <mbmyers@gmail.com>
2015-04-19 17:59:26 -0500

    updated feature cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,27 @@
 var blurbOverlay = ".blurbOverlay";
 
-jQuery(document).ready(function(){
+//jQuery(document).ready(function(){
 	// 'catTopPosition' is the amount of pixels #cat
 	// is from the top of the document
-	var caseStudy_1 = jQuery('#caseStudy_1').offset().top;
-	var caseStudy_2 = jQuery('#caseStudy_2').offset().top;
+//	var caseStudy_1 = jQuery('#caseStudy_1').offset().top;
+//	var caseStudy_2 = jQuery('#caseStudy_2').offset().top;
 	
 	// When #scroll is clicked
-	jQuery('#one').click(function(){
+//	jQuery('#one').click(function(){
 		// Scroll down to 'catTopPosition'
-		jQuery('html, body').animate({scrollTop:caseStudy_1}, 'slow');
+//		jQuery('html, body').animate({scrollTop:caseStudy_1}, 'slow');
 		// Stop the link from acting like a normal anchor link
-		return false;
-	});
+//		return false;
+//	});
     
     
-    	jQuery('#two').click(function(){
+//    	jQuery('#two').click(function(){
 		// Scroll down to 'catTopPosition'
-		jQuery('html, body').animate({scrollTop:caseStudy_2}, 'slow');
+//		jQuery('html, body').animate({scrollTop:caseStudy_2}, 'slow');
 		// Stop the link from acting like a normal anchor link
-		return false;
-	});
-});
+//		return false;
+//	});
+//});
 
 
 $(".myButton").hover(function() {
@@ -31,7 +31,14 @@ $(".myButton").hover(function() {
 
 $("#one").hover(function() {
     $(this).find(blurbOverlay).toggleClass("blurbMove");
-    $(this).find(imageStyling).toggleClass("hoverShadow");
+});
+
+$("#one").click(function() {
+    $(this).toggleClass("blockGrow");
+});
+
+$(".block").hover(function() {
+    $(this).toggleClass("blockLift");
 });
 
 $("#two").hover(function() {
