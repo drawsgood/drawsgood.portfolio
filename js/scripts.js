fb101b45f79ae3569f8d5085354f8b6e7fe2bd3a
drawsgood <mbmyers@gmail.com>
2015-04-18 22:48:12 -0500

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+var blurbOverlay = ".blurbOverlay";
+
+jQuery(document).ready(function(){
+	// 'catTopPosition' is the amount of pixels #cat
+	// is from the top of the document
+	var caseStudy_1 = jQuery('#caseStudy_1').offset().top;
+	var caseStudy_2 = jQuery('#caseStudy_2').offset().top;
+	
+	// When #scroll is clicked
+	jQuery('#one').click(function(){
+		// Scroll down to 'catTopPosition'
+		jQuery('html, body').animate({scrollTop:caseStudy_1}, 'slow');
+		// Stop the link from acting like a normal anchor link
+		return false;
+	});
+    
+    
+    	jQuery('#two').click(function(){
+		// Scroll down to 'catTopPosition'
+		jQuery('html, body').animate({scrollTop:caseStudy_2}, 'slow');
+		// Stop the link from acting like a normal anchor link
+		return false;
+	});
+});
+
+
+$(".myButton").hover(function() {
+    $(this).toggleClass("buttonOn");
+    $(this).children(".buttonText").toggleClass("textOn");
+});
+
+$("#one").hover(function() {
+    $(this).find(blurbOverlay).toggleClass("blurbMove");
+});
+
+$("#two").hover(function() {
+    $(this).find(blurbOverlay).toggleClass("blurbMove");
+});
+
+$("#three").hover(function() {
+    $(this).find(blurbOverlay).toggleClass("blurbMove");
+});
+
